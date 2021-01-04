6fdaeffba068ec459e2a3283efccaf1ccb3f45c5
Omar Joya <ojoyar@gmail.com>
2021-01-04 11:17:25 -0600

    Add association between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker_symbol, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -11,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       nil      
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker_symbol: ticker_symbol).first
   end
 end
